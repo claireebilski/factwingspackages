730a947c307d92ffa43343b8bf18ac0b472e8c80
claireebilski <35465034+claireebilski@users.noreply.github.com>
2018-10-15 11:45:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require('express')
 var app = express()
-var cors = require('cors')
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/factwings');
@@ -19,14 +18,7 @@ app.get('/factbook/:id', function(req, res) {
     });
 })
 
-app.get('/restaurants/v1/burgers/:city', cors(), function(req, res) {
-    var db = req.db;
-    var collection = db.get('restaurants');
-    collection.find({ "type": "burger", "branches.city": req.params.city }, {}, function(e, docs) {
-        res.send(docs);
-    });
-})
-
+app.set('json spaces', 2);
 
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!')
